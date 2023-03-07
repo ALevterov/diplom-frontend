dc3fe706614b136fc0e4742cd6bbd8950c367169
Alexander Levterov <shuroffrashiday@yandex.ru>
2023-03-07 13:30:32 +0300

    nihuya ne sdelal

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,10 +1,37 @@
+import jwtDecode from 'jwt-decode'
 import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
+
+export const verifyToken: Function = (token: string): Boolean => {
+  if (!token) {
+    return false
+  }
+  const decoded: { exp: number } = jwtDecode(token)
+  return decoded.exp > Date.now() / 1000
+}
+
+export const setSession = (access: string): void => {
+  if (access) {
+    localStorage.setItem('token', access)
+    axios.defaults.headers.common.Authorization = `Bearer ${access}`
+  } else {
+    localStorage.removeItem('token')
+    localStorage.removeItem('refresh'), localStorage.removeItem('users')
+    delete axios.defaults.headers.common.Authorization
+  }
+}
 
 const AuthProvider = ({ children }: { children: React.ReactElement }) => {
   const navigate = useNavigate()
   let isAuth = false
   React.useEffect(() => {
+    const init = async () => {
+      try {
+        const access = window.localStorage.getItem('access')
+        if (access && verifyToken(access)) {
+        }
+      } catch (e) {}
+    }
     if (!isAuth) {
       navigate('/')
     } else {
---
+++ src/utils/axios.ts
@@ -0,0 +1,5 @@
+let baseURL: string = ((process.env.REACT_APP_API_URL as string) +
+  process.env.REACT_APP_API_PREFIX) as string
+console.log(baseURL)
+
+const axios
